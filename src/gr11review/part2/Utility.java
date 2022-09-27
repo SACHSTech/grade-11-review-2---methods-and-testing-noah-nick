c13d57705990fbc975046b2597cadb315abb6308
NicholasKhatch <nicholas.khatchadourian23@ycdsbk12.ca>
2022-09-27 14:03:08 -0400

    complete method 3

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -2,7 +2,23 @@ package gr11review.part2;
 
 public class Utility {
 
-    
+    public static void main (String[] args){
+        System.out.print(xyzMiddle("aaxyzbb"));
+    }
 
+    public static boolean xyzMiddle(String str){
+        if (str.contains("xyz")){
+            int intLeftSide = str.indexOf("xyz") + 1;
+            int intRightSide = str.length() - (intLeftSide +1);
+            int intDifference = intLeftSide - intRightSide;
+            if (intDifference == -1 || intDifference == 0 || intDifference == 1){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
     
 }
